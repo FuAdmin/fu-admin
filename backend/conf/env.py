899909f6e763f736878949520b8d31dcc75b3929
jzangc <jzangc@jci.com>
2022-10-30 18:04:42 +0800

    修改配置文件

--- backend/conf/env.py
+++ backend/conf/env.py
@@ -8,9 +8,9 @@ DATABASE_HOST = "127.0.0.1"
 # 数据库端口
 DATABASE_PORT = 3306
 # 数据库用户名
-DATABASE_USER = "Fuadmin"
+DATABASE_USER = "root"
 # 数据库密码
-DATABASE_PASSWORD = "qazwsx"
+DATABASE_PASSWORD = ""
 # 数据库名
 DATABASE_NAME = "fuadmin"
 
@@ -29,4 +29,4 @@ LOGIN_NO_CAPTCHA_AUTH = True  # 登录接口 /api/token/ 是否需要验证码�
 ENABLE_LOGIN_ANALYSIS_LOG = True  # 启动登录详细概略获取(通过调用api获取ip详细地址)
 
 # 是否开启演示环境，开启后增 删 改功能失效
-DEMO = True
+DEMO = False
