8d6b480f569ca308c5c0bab1be5a98d8e2fd9b4a
dlpuzcl <dlpuzcl@163.com>
2023-04-23 09:56:59 +0800

    修复bug

--- backend/fuadmin/api.py
+++ backend/fuadmin/api.py
@@ -7,11 +7,10 @@ from ninja.security import APIKeyQuery, APIKeyHeader, HttpBearer, HttpBasicAuth
 
 from demo.router import demo_router
 from system.router import system_router
-from utils.fu_auth import AuthBearer, BasicAuth
-
+from utils.fu_auth import GlobalAuth
 from utils.fu_ninja import FuNinjaAPI
 
-api = FuNinjaAPI(auth=[AuthBearer(), BasicAuth()])
+api = FuNinjaAPI(auth=GlobalAuth)
 
 
 # 统一处理server异常
