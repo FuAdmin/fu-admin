3e25f2dc8ce2e028dd6374a116845fd9de8da2a6
狸花大白猫 <8328869+LHDBM@user.noreply.gitee.com>
2023-12-07 11:03:31 +0000

    修复登录提示错误，无法正确取值

--- web/src/api/sys/model/userModel.ts
+++ web/src/api/sys/model/userModel.ts
@@ -27,10 +27,10 @@ export interface GetUserInfoModel {
   roles: RoleInfo[];
   // 用户id
   userId: string | number;
-  // 用户名
+  // 用户账号
   username: string;
-  // 真实名字
-  realName: string;
+  // 用户姓名
+  name: string;
   // 头像
   avatar: string;
   // 介绍
