189deea1739ff1324b6c92fe701a8d59ee344aac
dlpuzcl <dlpuzcl@jci.com>
2024-04-09 20:31:57 +0800

    修复生成代码时后端代码无法生成的问题

--- backend/utils/usual.py
+++ backend/utils/usual.py
@@ -42,7 +42,7 @@ def get_dept(dept_id: int, dept_all_list=None, dept_list=None):
 def insert_content_after_line(filename, target_line, content_to_insert):
     try:
         # 打开文件并读取内容
-        with open(filename, 'r') as file:
+        with open(filename, 'r', encoding='utf-8') as file:
             lines = file.readlines()
 
         # 找到目标行的索引位置
