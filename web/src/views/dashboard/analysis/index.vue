1dcdaa8e21247f86f60e95a7d37dd41ce1c97aed
dlpuzcl <dlpuzcl@jci.com>
2024-08-15 21:22:27 +0800

    pro版提示

--- web/src/views/dashboard/analysis/index.vue
+++ web/src/views/dashboard/analysis/index.vue
@@ -8,6 +8,21 @@
       <SalesProductPie class="md:w-1/3 w-full" :loading="loading" />
     </div>
   </div>
+  <Modal
+    :closable="false"
+    v-model:open="open"
+    title="Fu Admin Pro"
+    @ok="handleOk"
+    centered
+    cancel-text="关闭"
+    ok-text="立即体验"
+  >
+    <div style="padding: 10px 20px">
+      <b>Fu Admin Pro</b> 已发布， 体验地址：<a :href="url" target="_blank">
+        {{ url }}
+      </a>
+    </div>
+  </Modal>
 </template>
 <script lang="ts" setup>
   import { ref } from 'vue';
@@ -16,10 +31,19 @@
   import VisitSource from './components/VisitSource.vue';
   import VisitRadar from './components/VisitRadar.vue';
   import SalesProductPie from './components/SalesProductPie.vue';
+  import { Modal } from 'ant-design-vue';
 
   const loading = ref(true);
+  const open = ref(true);
+
+  const url = 'http://124.222.210.96:6060';
 
   setTimeout(() => {
     loading.value = false;
   }, 1500);
+
+  const handleOk = () => {
+    window.location.href = url;
+    open.value = false;
+  };
 </script>
