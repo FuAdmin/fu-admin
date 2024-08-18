3781379ab79b03b63449f52eed8bf9e1383c0121
dlpuzcl <dlpuzcl@jci.com>
2024-08-18 20:38:13 +0800

    修复菜单刷新后才显示的BUG

--- web/src/views/dashboard/analysis/index.vue
+++ web/src/views/dashboard/analysis/index.vue
@@ -1,28 +1,30 @@
 <template>
-  <div class="p-4">
-    <GrowCard :loading="loading" class="enter-y" />
-    <SiteAnalysis class="!my-4 enter-y" :loading="loading" />
-    <div class="md:flex enter-y">
-      <VisitRadar class="md:w-1/3 w-full" :loading="loading" />
-      <VisitSource class="md:w-1/3 !md:mx-4 !md:my-0 !my-4 w-full" :loading="loading" />
-      <SalesProductPie class="md:w-1/3 w-full" :loading="loading" />
+  <div>
+    <div class="p-4">
+      <GrowCard :loading="loading" class="enter-y" />
+      <SiteAnalysis class="!my-4 enter-y" :loading="loading" />
+      <div class="md:flex enter-y">
+        <VisitRadar class="md:w-1/3 w-full" :loading="loading" />
+        <VisitSource class="md:w-1/3 !md:mx-4 !md:my-0 !my-4 w-full" :loading="loading" />
+        <SalesProductPie class="md:w-1/3 w-full" :loading="loading" />
+      </div>
     </div>
+    <Modal
+      :closable="false"
+      v-model:open="open"
+      title="Fu Admin Pro"
+      @ok="handleOk"
+      centered
+      cancel-text="关闭"
+      ok-text="立即体验"
+    >
+      <div style="padding: 10px 20px">
+        <b>Fu Admin Pro</b> 已发布， 体验地址：<a :href="url" target="_blank">
+          {{ url }}
+        </a>
+      </div>
+    </Modal>
   </div>
-  <Modal
-    :closable="false"
-    v-model:open="open"
-    title="Fu Admin Pro"
-    @ok="handleOk"
-    centered
-    cancel-text="关闭"
-    ok-text="立即体验"
-  >
-    <div style="padding: 10px 20px">
-      <b>Fu Admin Pro</b> 已发布， 体验地址：<a :href="url" target="_blank">
-        {{ url }}
-      </a>
-    </div>
-  </Modal>
 </template>
 <script lang="ts" setup>
   import { ref } from 'vue';
